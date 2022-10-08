6e6cc9afa3f5f8686aeaba765855ce4f84f6bd25
LHMOI SERVER <lol@mgail.com>
2022-10-07 21:35:40 -0300

    updates donut

--- create_sample.py
+++ create_sample.py
@@ -16,9 +16,9 @@ def main():
         _counter = 0
         for img, label in zip(df.data[s]['image'], df.data[s]['ground_truth']):
             im = Image.open(io.BytesIO(img[0].as_buffer()))
-            save_path = f'/home/lhmoi/pojetos/ml_lib/donut/cord_dataset_sample/{s}/img__{_counter}.jpg'
+            save_path = f'cord_dataset_sample/{s}/img__{_counter}.jpg'
             im.save(save_path)
-            with open(f'/home/lhmoi/pojetos/ml_lib/donut/cord_dataset_sample/{s}/metadata.jsonl', 'a+') as f:
+            with open(f'cord_dataset_sample/{s}/metadata.jsonl', 'a+') as f:
                 _jsonl = {'file_name': os.path.basename(save_path), 'ground_truth': json.dumps(json.loads(label.as_py()))}
                 f.write(json.dumps(_jsonl) + "\n")
             _counter += 1
